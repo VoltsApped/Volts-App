c166ae02e27e765ebaa9e4f1ec6eb4e6671008da
VoltsApped <VoltsApped@outlook.com>
2024-08-18 13:50:52 +0300

    Add files via upload

---
+++ app/desktop/page.tsx
@@ -0,0 +1,10 @@
+const DesktopPage = () => {
+	return (
+		<main className="min-h-screen flex items-center justify-center">
+			You can only play WhatTap from your cell mobile.
+			Играть в WhatTap можно только с телефона.
+		</main>
+	);
+};
+
+export default DesktopPage;
