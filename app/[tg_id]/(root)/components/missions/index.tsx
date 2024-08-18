99e08924c21e17b5d8746b4d508c72fd2a30f21d
VoltsApped <VoltsApped@outlook.com>
2024-08-18 16:35:34 +0300

    Update index.tsx

--- app/[tg_id]/(root)/components/missions/index.tsx
+++ app/[tg_id]/(root)/components/missions/index.tsx
@@ -41,7 +41,7 @@ const Missions = ({
 					id: "grouptg",
 					title: "Join our #VoltS TG channel",
 					reward: 50000,
-					imageUrl: "https://imgur.la/images/2024/08/18/image3e1c3ff0d1d3a0e2.png",
+					imageUrl: "/camping.png",
 					href: "https://t.me/VoltsApp",
 					channelId: -1002175966744,
 					type: "telegram",
